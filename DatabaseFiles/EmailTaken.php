d3dac400fad32a48e3e6a65b18e9750bd917b711
Bruce Casillas <154943317+Bruce562@users.noreply.github.com>
2025-01-24 00:14:29 -0500

    Update EmailTaken.php

--- DatabaseFiles/EmailTaken.php
+++ DatabaseFiles/EmailTaken.php
@@ -1,6 +1,6 @@
 <?php
 	
-	$con = mysqli_connect("localhost", "id15534403_snapchatdatabasetest", "Password123456789!", "id15534403_snapchatdatabase");
+	$con = mysqli_connect("localhost", "", "", "");
 	
 	$email = $_POST["email"];
 	
